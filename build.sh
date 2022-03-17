0edf72c790fa539fe3ae1a07fbb6f9befe93206e
phil294 <philip@waritschlager.de>
2022-03-17 20:09:29 +0100

    add ff amo zips generation

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
-yarn
 yarn coffee -c *.coffee
-rm -rf dist google-stinks
+yarn
+rm -rf dist* google-stinks
 mkdir google-stinks && cp node_modules/webextension-polyfill/dist/browser-polyfill.min.js google-stinks/
 mkdir dist
 cp -r manifest.json icons *.js *.html google-stinks dist
+
+# for FF AMO
+pushd dist
+zip -r dist.zip -- *
+popd
+git clone --depth=1 --branch=main . dist_src
+rm -rf ./dist_src/{.git,demo,icons,LICENSE}
+cd dist_src
+zip -r dist_src.zip -- *
